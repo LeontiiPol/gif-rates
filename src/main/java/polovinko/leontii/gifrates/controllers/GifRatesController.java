f65e1613172a5a6b148f63ff2110dc172966f8c0
Leontii Polovinko <half.leon11@gmail.com>
2022-06-05 23:33:38 +0300

    add swagger documentation

--- src/main/java/polovinko/leontii/gifrates/controllers/GifRatesController.java
+++ src/main/java/polovinko/leontii/gifrates/controllers/GifRatesController.java
@@ -1,11 +1,17 @@
 package polovinko.leontii.gifrates.controllers;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
+import polovinko.leontii.gifrates.dto.error.ErrorResponseBody;
 import polovinko.leontii.gifrates.dto.gif.Gif;
 import polovinko.leontii.gifrates.services.gif.GifRatesService;
 
@@ -16,8 +22,17 @@ public class GifRatesController {
 
   private final GifRatesService gifRatesService;
 
-  @GetMapping("/{currencyCode}")
-  public ResponseEntity<Gif> getCurrencyExchangeRateAsGif(@PathVariable final String currencyCode) {
+  @ApiOperation(value = "Returns random Gif depending on currency's exchange rate")
+  @ApiResponses({
+      @ApiResponse(code = 200, message = "Successful", response = Gif.class),
+      @ApiResponse(code = 404, message = "Bad request", response = ErrorResponseBody.class),
+      @ApiResponse(code = 500, message = "Internal server error", response = ErrorResponseBody.class)
+  })
+  @GetMapping(value = "/{currency-code}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Gif> getCurrencyExchangeRateAsGif(
+      @ApiParam(example = "RUB", value = "Three-letter currency code")
+      @PathVariable(name = "currency-code") final String currencyCode) {
+
     return ResponseEntity.ok()
         .body(gifRatesService.getCurrencyExchangeRateAsGif(currencyCode));
   }
--- src/main/java/polovinko/leontii/gifrates/dto/currency/CurrencyCodesCollection.java
+++ src/main/java/polovinko/leontii/gifrates/dto/currency/CurrencyCodesCollection.java
@@ -2,6 +2,7 @@ package polovinko.leontii.gifrates.dto.currency;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +17,7 @@ import java.util.Set;
 @JsonDeserialize(using = CurrencyCodesCollectionDeserializer.class)
 public class CurrencyCodesCollection {
 
+  @ApiModelProperty(value = "Alphabetically sorted array of unique currency codes", example = "[\"ARD\", \"BMD\"]")
   @JsonProperty(value = "codes")
   private Set<String> currencyCodes;
 }
